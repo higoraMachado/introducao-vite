557d5e1a75907edd4d0b462d90deb4341b15d26f
Higor Machado <higor.machado2011@gmail.com>
2026-08-06 22:49:26 -0300

    06/08

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import './App.css'
+import styles from './App.module.css'
 
 function App() {
   return (
     <div>
       <h1>Home</h1>
       <p>Exemplo paràgrafo</p>
-      <h2>Exemplo h2</h2>
+      <h2 className={styles.titulo2}>Exemplo h2</h2>
       <h3>Exemplo h3</h3>
       <label htmlFor=''>Exemplo label</label>
     </div>
