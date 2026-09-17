c9874460beea8899f155ce7a694468d55b25aa8e
HERA SILVEIRA RAMOS <hera.ramos>
2026-09-17 19:59:35 -0300

    colocando o diretorio

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/dashboardCliente/Dashboard';
 import Login from './pages/login';
 import Agendamento from './pages/agendamento';
+import Perfil from './pages/perfil/Perfil';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/dashboardCliente" element={<Dashboard />} />
       <Route path="/agendamento" element={<Agendamento />} />
+      <Route path="/perfil" element={<Perfil />} />
     </Routes>
   );
 }
